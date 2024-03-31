d7c02d70788ee37d6667345f6b5cd7ea9ce2d49c
Maroosh-Gillani <maroosh1171@gmail.com>
2024-03-31 19:36:43 -0400

    downloaded raw data from datasets

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,39 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto's "Deaths of People Experiencing Homelessness" and "Toronto Shelter System Flow" datasets.
+# Author: Maroosh Gillani
+# Date: March 31st 2024
+# Contact: maroosh.gillani@mail.utoronto.ca
+# Pre-requisites: N/A
+# Any other information needed? N/A
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+# Download data from Deaths of People Experiencing Homelessness
 
+package_1 <- show_package("a7ae08f3-c512-4a88-bb3c-ab40eca50c5e")
+
+resources_1 <- list_package_resources("a7ae08f3-c512-4a88-bb3c-ab40eca50c5e")
+
+datastore_resources_1 <- filter(resources_1, tolower(format) %in% c('csv', 'geojson'))
+
+homeless_death_data <- filter(datastore_resources_1, row_number()==1) %>% get_resource()
+
+# Download data from Toronto Shelter System Flow
+
+package_2 <- show_package("ac77f532-f18b-427c-905c-4ae87ce69c93")
+
+resources_2 <- list_package_resources("ac77f532-f18b-427c-905c-4ae87ce69c93")
+
+datastore_resources_2 <- filter(resources_2, tolower(format) %in% c('csv', 'geojson'))
+
+shelter_data <- filter(datastore_resources_2, row_number()==1) %>% get_resource()
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
-         
+write_csv(x=homeless_death_data, file="data/raw_data/raw_homeless_death_data.csv")
+write_csv(shelter_data, "data/raw_data/raw_shelter_data.csv")
