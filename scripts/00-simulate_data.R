6f934914f133dc4cf41ddec12c18b1338a03a92b
Maroosh-Gillani <maroosh1171@gmail.com>
2024-04-12 19:47:29 -0400

    added simulation

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,28 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulates number of homeless deaths and number of homeless people returning to shelters per year
+# Author: Maroosh Gillani
+# Date: 12 March 2024
+# Contact: maroosh.gillani@mail.utoronto.ca
+# Pre-requisites: N/A
+# Any other information needed? N/A
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(7)
+
+years <- 2018:2022
+
+returns <- seq(1000, 1500, length.out = length(years)) + rnorm(length(years), mean = 0, sd = 100)
+deaths <- returns * 0.02 + rnorm(length(years), mean = 0, sd = 20)
+
+# store simulated data in a dataframe
+yearly_data <- data.frame(Year = years,
+                   `Number of Homeless Deaths` = round(deaths),
+                   `Number of People Returning to Homeless Shelters` = round(returns))
+
 
 
 
