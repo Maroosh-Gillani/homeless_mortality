02912d16ef887af00b84e4d882a2da453f196455
Maroosh-Gillani <maroosh1171@gmail.com>
2024-04-15 16:25:33 -0400

    improved column names for clean data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -19,31 +19,19 @@ month_mapping <- c("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May
 
 # Cleaned death data with year and month
 cleaned_death_data <- raw_death_data %>%
-  # Select only the "Year of death", "Month of death", and "Count" columns
   select(`Year of death`, `Month of death`, Count) %>%
-  # Filter rows where the year is between 2018 and 2022
   filter(`Year of death` >= 2018 & `Year of death` <= 2022) %>%
-  # Group by "Year of death" and "Month of death" and summarize the counts
   group_by(`Year of death`, `Month of death`) %>%
-  # summarize(Count = sum(Count)) %>%
-  # Convert month names to numeric values using custom mapping
   mutate(Month = month_mapping[`Month of death`])
 
-
-# Cleaned shelter data with year and month
+# Cleaned shelter data
 cleaned_shelter_data <- raw_shelter_data %>%
-  # Select only the "date.mmm.yy.", "population_group", and "returned_to_shelter" columns
   select(date.mmm.yy., population_group, returned_to_shelter) %>%
-  # Filter rows where the year is between 2018 and 2022
   filter(as.integer(substring(date.mmm.yy., nchar(date.mmm.yy.) - 1)) >= 18 & as.integer(substring(date.mmm.yy., nchar(date.mmm.yy.) - 1)) <= 22) %>%
-  # Filter rows where the population_group is "All Population"
   filter(population_group == "All Population") %>%
-  # Extract year and month from "date.mmm.yy." column
   mutate(Year = paste0("20", substring(date.mmm.yy., nchar(date.mmm.yy.) - 1)),
          Month = match(substring(date.mmm.yy., 1, 3), month.abb)) %>%
-  # Select only the "Year", "Month", and "returned_to_shelter" columns
   select(Year, Month, returned_to_shelter) %>%
-  # Group by "Year" and "Month" and summarize the counts
   group_by(Year, Month) %>%
   summarize(returned_to_shelter = sum(returned_to_shelter))
 
@@ -62,6 +50,11 @@ clean_data <- clean_data %>%
 # Remove the separate Month and Year columns
 clean_data <- clean_data %>%
   select(-Year, -Month)
+
+# Rename columns
+clean_data <- clean_data %>%
+  rename(`Returned to shelter` = returned_to_shelter,
+         `Month/Year` = Year_Month)
 
 #### Save data ####
 write_csv(clean_data, "data/clean_data/cleaned_data.csv")
