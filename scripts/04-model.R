3ab01d45f28ebf688befc302655099b233b088f5
Maroosh-Gillani <maroosh1171@gmail.com>
2024-04-15 18:39:57 -0400

    added to model script + model rds file

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,37 +1,27 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Models the cleaned data derived from OpenDataToronto's datasets using poisson regression
+# Author: Maroosh Gillani
+# Date: 15 April 2024
+# Contact: maroosh.gillani@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: Make sure that 01-download_data.R and 02-data_cleaning.R have been run already.
 
 #### Workspace setup ####
 library(tidyverse)
+library(here)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+cleaned_data = read.csv(file=here("data/clean_data/cleaned_data.csv"))
 
 ### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
-
+poisson_model <- 
+  glm(Deaths ~ Returned.to.shelter, data = cleaned_data, family = poisson)
 
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+  poisson_model,
+  file = "models/poisson_model.rds"
 )
 
 
